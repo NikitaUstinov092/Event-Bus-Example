53d0eb61c7de93cedd1de56298baad9160f95927
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2023-12-21 22:03:50 +0300

    Добавил ресурсы

---
+++ Assets/Scripts/Turn/TurnPipelineInstaller.cs
@@ -0,0 +1,26 @@
+using Lessons.Tasks.Turn;
+using VContainer;
+using VContainer.Unity;
+
+namespace Lessons.Turn
+{
+    public sealed class TurnPipelineInstaller : IInitializable
+    {
+        private readonly TurnPipeline _turnPipeline;
+        private readonly IObjectResolver _objectResolver;
+        
+        public TurnPipelineInstaller(TurnPipeline turnPipeline, IObjectResolver objectResolver)
+        {
+            _turnPipeline = turnPipeline;
+            _objectResolver = objectResolver;
+        }
+        
+        void IInitializable.Initialize()
+        {
+            _turnPipeline.AddTask(new StartTurnTask());
+            _turnPipeline.AddTask(_objectResolver.CreateInstance<PlayerTurnTask>());
+            _turnPipeline.AddTask(_objectResolver.CreateInstance<VisualTurnTask>());
+            _turnPipeline.AddTask(new FinishTurnTask());
+        }
+    }
+}
